5576ebc9e188b4495a5b8574a67f7e2ab3d3054f
alla0243 <43067479+ChristianAllardAC@users.noreply.github.com>
2020-02-23 19:35:23 -1000

    added junk folder

---
+++ junk/foodEntrySample.js
@@ -0,0 +1,47 @@
+let obj = {
+      timeStamp: 1585002600000,
+      exerciseEntry: {
+        durationMins: 30,
+        notes: "Tough one",
+        title: "Treadmill",
+        type: "Cardio" 
+      },
+        foodEntry: {
+          dairy: {
+            name: "Dairy",
+            portions: 2
+          },
+          fats: {
+            name: "Fats",
+            portions: 7
+          },
+          fruit: {
+            name: "Fruit",
+            portions: 4
+          },
+          protein: {
+            name: "Protein",
+            portions: 12
+          },
+          resVegs: {
+            name: "Res. Vegetables",
+            portions: 1
+          },
+          simpleCarbs: {
+            name: "Simple Carbs",
+            portions: 2
+          }
+        },
+        moodEntry: {
+          diary: "Feeling good",
+          mood: 4
+        },
+        sleepEntry: {
+          end: 0,
+          start: 0,
+          notes: "Slept great"
+        },
+        waterEntry: {
+          portions: 8
+        }
+}
