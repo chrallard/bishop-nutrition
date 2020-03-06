6298dc8e392b55e1bfe7da43ce0a995ca86996d4
Durbano <durb0004@algonqiunlive.com>
2020-03-06 14:57:38 -0500

    updated to use different dropdown menu node pack

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, FlatList, Button, ScrollView, Item, SectionList } from 'react-native'
 import { List, Checkbox } from 'react-native-paper';
+import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
 import * as firebase from "firebase/app"
 import "firebase/firestore"
+let dairyPortions = 0
+let restrictedVegPortions = 0
+let fruitPortions = 0
+let simpleCarbPortions = 0
+let proteinPortions = 0
+let fatPortions = 0
+let freeVegPortions = 0
 
 export default class ProfileScreen extends Component {
+
 
   constructor(props) {
     super(props)
@@ -21,6 +31,14 @@ export default class ProfileScreen extends Component {
     this.state = {
       db: firebase.firestore(),
       expanded: false,
+      activeSections: [],
+      dairyPortions: 0,
+      restrictedVegPortions: 0,
+      fruitPortions: 0,
+      simpleCarbPortions: 0,
+      proteinPortions: 0,
+      fatPortions: 0,
+      freeVegPortions: 0,
       lists: [
         {
           type: "Dairy",
@@ -104,27 +122,120 @@ export default class ProfileScreen extends Component {
     this.setState({ lists: listArray })
   }
 
+
   _handlePress = (food) =>
     this.setState({
       expanded: !this.state.expanded
     });
+
+  _renderSectionTitle = section => {
+    return (
+
+      // console.log((section.type))
+      <View style={styles.content}>
+        <Text style={styles.content}>{}</Text>
+      </View>
+    );
+  };
+
+  _renderHeader = section => {
+    return (
+      <View style={styles.header}>
+
+        <Text style={{ fontSize: 24 }}>{section.type}</Text>
+      </View>
+    );
+  };
+
+  _addPortion = (item) => {
+    switch (item) {
+      case "Dairy":
+        dairyPortions++;
+        console.log(item)
+        break;
+      case "Restricted Vegetables":
+        restrictedVegPortions++;
+        console.log(item)
+        break;
+      case "Fruit":
+        fruitPortions++;
+        console.log(item)
+        break;
+      case "Simple Carbs":
+        simpleCarbPortions++;
+        console.log(item)
+        break;
+      case "Protein":
+        proteinPortions++;
+        console.log(item)
+        break;
+      case "Fats":
+        fatPortions++;
+        console.log(item)
+        break;
+      case "Free Vegetables":
+        freeVegPortions++;
+        console.log(item)
+        break;
+
+      default:
+        break;
+    }
+
+
+  }
+
+  _renderContent = section => {
+
+    return (
+      section.list.map((item, key) => (
+        <View key={key} style={styles.content}>
+          <Text >{item.name}</Text>
+          <Button title="Add" onPress={() => this._addPortion(item.category)} ></Button>
+        </View>
+      ))
+    );
+  };
+
+  _updateSections = activeSections => {
+    this.setState({ activeSections });
+  };
   render() {
     return (
-      <View>
 
-        <List.Section style={{ marginTop: 30 }} title="Food List">
-
-          {this.state.lists.map((item, key) => (
-            <List.Accordion key={key}
-              title={item.type}
-            >
-              {item.list.map((item, key) => (
-                <List.Item key={key} title={item.name} />
-              ))}
-            </List.Accordion>
-          ))}
-        </List.Section>
+      <View style={{ margin: 30, width: "auto" }}>
+        <Accordion
+          sections={this.state.lists}
+          activeSections={this.state.activeSections}
+          renderSectionTitle={this._renderSectionTitle}
+          renderHeader={this._renderHeader}
+          renderContent={this._renderContent}
+          onChange={this._updateSections}
+        />
       </View>
+
+
+
+
+
+
+
+
+      // <View>
+
+      //   <List.Section style={{ marginTop: 30 }} title="Food List">
+
+      //     {this.state.lists.map((item, key) => (
+      //       <List.Accordion key={key}
+      //         title={item.type}
+      //       >
+      //         {item.list.map((item, key) => (
+      //           <List.Item key={key} title={item.name} />
+      //         ))}
+      //       </List.Accordion>
+      //     ))}
+      //   </List.Section>
+      // </View>
 
 
       //#region failed attempts
