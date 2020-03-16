af51f4a291ac6045b914f7386a4089d09902260d
alla0243 <43067479+ChristianAllardAC@users.noreply.github.com>
2020-03-16 18:17:54 -0400

    added each empty widget

--- screens/SummaryScreen.js
+++ screens/SummaryScreen.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import NutritionWidget from '../widgets/summaryWidgets/NutritionWidget'
+import WaterWidget from '../widgets/summaryWidgets/WaterWidget'
+import ActivityWidget from '../widgets/summaryWidgets/ActivityWidget'
+import SleepWidget from '../widgets/summaryWidgets/SleepWidget'
+import MoodWidget from '../widgets/summaryWidgets/MoodWidget'
 
 export default class SummaryScreen extends Component {
 
@@ -13,7 +18,12 @@ export default class SummaryScreen extends Component {
   render() {
     return (
           <View style={styles.container} >
-            <Text>{this.props.route.params.docId}</Text>
+            {/* <Text>{this.props.route.params.docId}</Text> */}
+            <NutritionWidget />
+            <WaterWidget />
+            <ActivityWidget />
+            <SleepWidget />
+            <MoodWidget />
           </View>
     )
   }
---
+++ widgets/summaryWidgets/NutritionWidget.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, YellowBox } from 'react-native'
+
+export default class NutritionWidget extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentDidMount(){
+       
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.title}>Widget</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    // STYLING JEFF March 6
+    container:{
+        flexDirection: 'column',
+        backgroundColor: '#1C1C1E',
+        padding: 16,
+        alignSelf: 'stretch',
+        marginBottom: 8,
+        marginTop: 44
+    },
+    title:{
+        color:'#FAFAFA',
+        fontSize: 20,
+    },
+    nameText:{
+        flexDirection: 'column',
+        color:'#FAFAFA',
+        fontSize: 28,
+        justifyContent: 'center',
+        alignSelf:'center',
+        marginBottom: 8
+    },
+    dateText:{
+        color:'#347EFB',
+        fontSize: 17,
+        justifyContent: 'center',
+        alignSelf:'center'
+    }
+})
