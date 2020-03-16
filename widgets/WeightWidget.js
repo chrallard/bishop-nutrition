322931f6eac8bb80fa8b6ab18b1378b0af107ed0
Jeff Lichty <lich0020@algonquinlive.com>
2020-03-16 19:46:52 -0400

    styled weight widget

--- widgets/WeightWidget.js
+++ widgets/WeightWidget.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Dash from 'react-native-dash'
 import * as firebase from "firebase/app"
 import "firebase/firestore"
 import 'firebase/auth'
@@ -35,7 +36,8 @@ export default class WelcomeWidget extends Component{
             
              this.setState({weightEntry : doc.data().weightEntry}); 
         })
-       this.setState({subtract: (this.state.startingWeight) - (this.state.weightEntry)})
+    //    this.setState({subtract: (this.state.startingWeight) - (this.state.weightEntry)})
+        this.setState({subtract: (this.state.weightEntry) - (this.state.startingWeight)})
     }
    
 
@@ -43,12 +45,32 @@ export default class WelcomeWidget extends Component{
         return(
             <View style={styles.container}>
            
-            <Text>Weight</Text>
+                <Text style={styles.titleText}>Weight</Text>
             
-             <Text>{this.state.startingWeight} -------{this.state.subtract}----------{this.state.weightEntry}</Text>
-             {/* <Text style={styles.difference}>{this.state.subtract}</Text>
-             <Text style={styles.weight}>{this.state.weightEntry}</Text>
-            */}
+                <View style={styles.textRow}>
+                    {/* <Text style={styles.startingText}>{this.state.startingWeight} -------{this.state.subtract}----------{this.state.weightEntry}</Text> */}
+                    {/* <Text style={styles.difference}>{this.state.subtract}</Text>
+                    <Text style={styles.weight}>{this.state.weightEntry}</Text>
+                    */}
+                    <View style={styles.itemBox}>
+                        <Text style={styles.startingText}>{this.state.startingWeight}</Text>
+                        <Text style={styles.bodyTextStart}>Starting</Text>
+                    </View>
+
+                    <Dash dashColor='#347EFB' dashGap={7} dashLength={10} style={{width:70, height:10}} />
+
+                    <View style={styles.itemBox}>
+                        <Text style={styles.differenceText}>{this.state.subtract}</Text>
+                        <Text style={styles.bodyTextProgress}>Progress</Text>
+                    </View>
+
+                    <Dash dashColor='#347EFB' dashGap={7} dashLength={10} style={{width:70, height:10}} />
+
+                    <View style={styles.itemBox} >
+                        <Text style={styles.currentText}>{this.state.weightEntry}</Text>
+                        <Text style={styles.bodyTextCurrent}>Current</Text>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -57,24 +79,119 @@ export default class WelcomeWidget extends Component{
 }
 
 const styles = StyleSheet.create({
+    //Styled by Jeff March 16th
     container:{
-        backgroundColor: '#fff',
-        alignItems: 'flex-start',
-        paddingTop: 10,
-        paddingBottom: 10,
-        alignSelf: 'stretch',
-        marginBottom: 20,
-        marginTop: 20
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: '#1C1C1E',
+      padding: 16,
+      alignSelf: 'stretch',
+      marginBottom: 8,
+      marginTop: 8
+    },
+    titleText:{
+      color:'#FAFAFA',
+      fontSize: 20,
+      //marginBottom: 16
+    },
+    textRow:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems:'center',
+        //height: 40
     },
 
-    // weight:{
-        
-    //     flexDirection:'row',
-    //     alignSelf:"flex-end",
-       
-       
-    // },
-    // difference:{
-    //     alignSelf:"center"
-    // }
+    startingText:{
+      color:'#347EFB',
+      fontSize: 20,
+      alignSelf:'center',
+    },
+    differenceText:{
+        color:'#00DCF1',
+        fontSize: 40,
+        alignSelf:'center',
+    },
+    currentText:{
+        color:'#347EFB',
+        fontSize: 26,
+        alignSelf:'center',
+    },
+    dash:{
+        color: '#DDDEDE',
+        width: 120,
+        height:10,
+
+
+    },
+
+
+    itemBox:{
+        height: 50,
+        flexDirection:'column',
+        alignItems: 'center',
+        //backgroundColor: 'red',
+        justifyContent: 'flex-end',
+    },
+      bodyTextStart:{
+      color:'#DDDEDE',
+      fontSize: 8,     
+      marginTop: 8
+    },
+    bodyTextProgress:{
+        color:'#DDDEDE',
+        fontSize: 8,     
+    },
+    bodyTextCurrent:{
+        color:'#DDDEDE',
+        fontSize: 8,     
+        marginTop: 4
+    },
+
+
 })
+
+
+{/* <Image source={ this.state.flagImage2 === true ?                  
+                          require('../Images/empty_Cup.jpg')  : require('../Images/full_Cup1.png')}
+                          style={styles.image}
+           /> */}
+
+// const styles = StyleSheet.create({
+
+//     //Styled by Jeff March 6th
+//     container:{
+//       display: 'flex',
+//       flexDirection: 'column',
+//       backgroundColor: '#1C1C1E',
+//       padding: 16,
+//       alignSelf: 'stretch',
+//       marginBottom: 8,
+//       marginTop: 8
+//   },
+  
+//     titleContainer:{
+//       flexDirection: 'row',
+//       alignItems: 'center',
+//       justifyContent: 'space-between',
+//       marginBottom: 8
+//   },
+//     titleText:{
+//       color:'#FAFAFA',
+//       fontSize: 20,
+//   },
+//   bodyText:{
+//       color:'#DDDEDE',
+//       fontSize: 12,
+//   },
+  
+//   cupRow:{
+//     flexDirection: 'row',
+//     justifyContent: 'space-between'
+//   },
+//   image: {
+//         height: 45,
+//         width: 40,
+//         resizeMode: 'cover',     
+//         alignItems: 'stretch'    
+//     }
+//   });
