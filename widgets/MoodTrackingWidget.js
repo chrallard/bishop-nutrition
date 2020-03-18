b3e733b09fb83f8c0531c1e39d08382f478e5880
Jeff Lichty <lich0020@algonquinlive.com>
2020-03-18 11:53:47 -0400

    styling done

--- widgets/MoodTrackingWidget.js
+++ widgets/MoodTrackingWidget.js
@@ -32,185 +32,166 @@ export default class MoodTrackingWidget extends Component {
         
         <View style={styles.container}>
            
-            <View style={{flexDirection:'row'}}>
-         <Text>Mood</Text>
-         </View>
-         <TouchableOpacity onPress={()=>{
-             this.setState({
-             showMe:true
-         })}
-         }> 
+            <View>
+                <Text  style={styles.title}>Mood</Text>
+            </View>
+
+            <TouchableOpacity onPress={()=>{
+                this.setState({
+                showMe:true
+                })}
+            }> 
         
-         <View style={styles.image1}>
-         
-                   <Image source={require('../Images/smile_1.png')} style={{width: 60, height: 60,margin:5}}/>
-                  
-                   <Image source={require('../Images/smile_2.png')} style={{width: 60, height: 60,margin:5}}/>
-                   <Image source={require('../Images/smile_3.png')} style={{width: 60, height: 60,margin:5}}/>
-                   </View>
-                   <View style={styles.image2}>
-                   <Image source={require('../Images/smile_4.png')} style={{width: 60, height: 60,margin:5}}/>
-                   <Image source={require('../Images/smile_5.png')} style={{width: 60, height: 60,margin:5}}/>
-                   <Image source={require('../Images/smile_6.png')} style={{width: 60, height: 60,margin:5}}/>
-                   </View>
-                   </TouchableOpacity>
+                <View style={styles.imageRow1}>            
+                    <Image source={require('../Images/smile_1.png')} style={styles.emoji}/>
+                    <Image source={require('../Images/smile_2.png')} style={styles.emoji}/>
+                    <Image source={require('../Images/smile_3.png')} style={styles.emoji}/>
+                </View>
+                <View style={styles.imageRow2}>
+                    <Image source={require('../Images/smile_4.png')} style={styles.emoji}/>
+                    <Image source={require('../Images/smile_5.png')} style={styles.emoji}/>
+                    <Image source={require('../Images/smile_6.png')} style={styles.emoji}/>
+                </View>
+
+            </TouchableOpacity>
 
 
    
                    
-                   <Modal visible={this.state.showMe} animationType={'slide'}>
-                       <View style={styles.modalStyle}>
-                   <View style={styles.modalHeader}>
-                   <TouchableOpacity
-            onPress={()=>{
-                   this.setState({
-                       showMe:false
-                   })
-               }}>
-             <Text style={styles.modalBack}>Back</Text>
-             </TouchableOpacity>
+            <Modal visible={this.state.showMe} animationType={'slide'}>
 
-             <Text style={styles.modalTitle}>Mood</Text>
-             
-            
-            
-            
-             <TouchableOpacity
-             onPress={()=>{
-                   this.setState({
-                       showMe:false
-                   })
-               }}>
-             <Text style={styles.modalSave}>Save</Text>
-             </TouchableOpacity>
-             
-             
+                <View style={styles.modalStyle}>
+                    <View style={styles.modalHeader}>
 
-              <View style={styles.modalSeprateLine}/> 
-         </View>
-         <View>
-            <Text style={{paddingTop:20}}>How is Your Mood Today:</Text>
-        </View>
+                    <TouchableOpacity onPress={()=>{ this.setState({
+                        showMe:false
+                        })}}>
+                        <Text style={styles.modalNav}>Back</Text>
+                    </TouchableOpacity>
+
+                    <Text style={styles.modalTitle}>Mood</Text>
+                
+                    <TouchableOpacity onPress={()=>{ this.setState({
+                        showMe:false
+                        })}}>
+                        <Text style={styles.modalNav}>Save</Text>
+                    </TouchableOpacity>
+             
+                    <View style={styles.modalSeprateLine}/> 
+
+                    </View>
+
+
+
+                    <View>
+                        <Text style={styles.content}>How is Your Mood Today:</Text>
+                    </View>
+
           
-         <View style={styles.imageM1}> 
-        <Image source={require('../Images/smile_1.png')} style={{width: 65, height: 65,margin:10}}/>
-        <Image source={require('../Images/smile_2.png')} style={{width: 65, height: 65,margin:10}}/>
-        <Image source={require('../Images/smile_3.png')} style={{width: 65, height: 65,margin:10}}/>
-        </View>
-        <View style={styles.imageM2}>
-        <Image source={require('../Images/smile_4.png')} style={{width: 65, height: 65,margin:10}}/>
-         <Image source={require('../Images/smile_5.png')} style={{width: 65, height: 65,margin:10}}/>
-        <Image source={require('../Images/smile_6.png')} style={{width: 65, height: 65,margin:10}}/>
-        </View>
-        <View>
-                       <Text>Diary:</Text>
-                   </View>
-                   <View style={styles.modalView}> 
-                       <TextInput style = {styles.input}
-                        underlineColorAndroid = "transparent"
-                        multiline={true}
-                        numberOfLines={4}
-                        placeholderTextColor = "#9a73ef"
-                        autoCapitalize = "none"
-                        onChangeText={(text) => this.setState({Text:text})}
-                        value={this.state.Text}/>
-               
-                   </View>
-                   </View>
-                  </Modal>
+                    <View style={styles.imageRow1}> 
+                        <Image source={require('../Images/smile_1.png')} style={styles.emoji}/>
+                        <Image source={require('../Images/smile_2.png')} style={styles.emoji}/>
+                        <Image source={require('../Images/smile_3.png')} style={styles.emoji}/>
+                    </View>
 
-                </View>   
+                    <View style={styles.imageRow2}>
+                        <Image source={require('../Images/smile_4.png')} style={styles.emoji}/>
+                        <Image source={require('../Images/smile_5.png')} style={styles.emoji}/>
+                        <Image source={require('../Images/smile_6.png')} style={styles.emoji}/>
+                    </View>
+
+                    <View>
+                        <Text style={styles.content}>Diary:</Text>
+                    </View>
+
+                    <View>
+                        <TextInput style = {styles.modalInput}
+                            underlineColorAndroid = "transparent"
+                            multiline={true}
+                            numberOfLines={4}
+                            placeholderTextColor = '#B7B7B7'
+                            autoCapitalize = "none"
+                            onChangeText={(text) => this.setState({Text:text})}
+                            value={this.state.Text}/>
+                    </View>
+                </View>
+            </Modal>
+        </View>   
 )
 }; 
     };
 
 const styles = StyleSheet.create({
-    container: {
-       display:"flex",
-       backgroundColor: '#fff',
+    //Styled by Jeff March 18
+    container:{
+        display: 'flex',
+        backgroundColor: '#1C1C1E',
         alignSelf: 'stretch',
-        },
-    
-        modalStyle:{
-        justifyContent: 'center',
-        width: screen.width ,
-        height:520
+        marginBottom: 8,
+        marginTop: 8,
+        padding: 16,
     },
-   
+    title:{
+        color:'#FAFAFA',
+        fontSize: 20,
+        marginBottom: 16
+    },
+    emoji:{
+        height: 60,
+        width: 60
+    },
+    imageRow1: {
+        flexDirection:"row",
+        justifyContent: 'space-around',
+        marginBottom: 16
+    },
+    imageRow2:{
+        flexDirection:"row",
+        justifyContent: 'space-around',
+        marginBottom: 16
+    },
+    content:{
+        marginTop: 24,
+        marginBottom: 16,
+        color: '#DDDEDE',
+        fontSize: 17,
+    },
+    modalInput: {
+        height: 200,
+        borderColor: '#B7B7B7',
+        borderWidth: 1,
+        backgroundColor: '#2C2C2E',
+        borderRadius: 8
+    },
+    modalStyle:{
+        padding: 16,
+        height: '100%',
+        backgroundColor: '#0D0D0D',
+        marginTop: 88,
+        borderRadius: 25
+    },   
     modalHeader:{
-        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignContent: 'center',
+        marginBottom: 16
+    },
+    modalTitle:{
+        color: '#FAFAFA',
+        fontSize: 20,
+        fontWeight: '700',
+    },
+    modalNav:{
+        fontSize: 17,
+        color: '#347EFB',
+    },
+    modalSeprateLine:{
         width:'100%',
-        height: '5%',
-       flexDirection:'row',
-       alignItems:'center',
-justifyContent:'center',
-},
-   
-modalTitle:{
-    color:'black',
-    marginBottom:4,
-    fontSize:16,
-    fontWeight:'bold',
-    flex: 1,
-   paddingLeft:150,
-  
-},
-
-modalSave:{
-    color:'black',
-    fontSize:16,
-    fontWeight:'bold',
-    flex: 1,
-    
-    textAlign: 'right',
-    paddingRight:10
-},
-
-modalBack:{
-    color:'black',
-    fontSize:16,
-    fontWeight:'bold',
-    flex: 1,
-    textAlign: 'left',
-    paddingLeft:10,
-},
-  
-modalSeprateLine:{
-width:'100%',
-height:'2%',
-position:'absolute',
-backgroundColor:'black',
-bottom:0
-   },
-    
-   image1: {
-     display:"flex",
-     flexDirection:"row",
-     justifyContent:'space-between',
-      },
-    image2:{
-    display:"flex",
-    flexDirection:"row",
-    justifyContent:'space-between',
-   },
-   imageM1: {
- 
-    flexDirection:"row",
-    justifyContent:'space-between',
-    marginBottom:10,
-     },
-   imageM2:{
-
-   flexDirection:"row",
-   justifyContent:'space-between',
-   marginBottom:10,
-  },
-  input: {
-   margin: 15,
-   height: 100,
-   borderColor: '#000000',
-   borderWidth: 1
-},
+        height: 0.4,
+        position:'absolute',
+        backgroundColor:'#DDDEDE',
+        bottom:-16,
+    },
 
 });
 
