71d62e6e038b99eb7cbab3e93e2e08f5f09f4b4e
Brinda <brindashah999@gmail.com>
2020-03-22 13:03:23 -0400

    just added the timestamp

--- screens/ProgressScreen.js
+++ screens/ProgressScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View , FlatList} from 'react-native'
+import { StyleSheet, Text, View , FlatList, ScrollView} from 'react-native'
+import SegmentedControlTab from "react-native-segmented-control-tab";
 import * as firebase from "firebase/app"
 import "firebase/firestore"
 import 'firebase/auth'
+
 
 
 export default class ProgressScreen extends Component{
@@ -14,7 +16,9 @@ export default class ProgressScreen extends Component{
            data:[],
            timeStamp: [],
            startingWeight: [],
-           difference: []
+           selectedIndex: 0,
+           time: []
+          
            
       }
     }
@@ -23,9 +27,40 @@ export default class ProgressScreen extends Component{
        await  this.list()
        await this.time()
        await this.startingWeight()
-       await this.progress()
+      // await this.progress()
+       await this.handleIndexChange()
       
     }
+
+    handleIndexChange = async (index) => {
+        this.setState({
+          ...this.state,
+          selectedIndex: index,
+        });
+
+        if(index == 1){
+            let uid = await firebase.auth().currentUser.uid
+            await firebase.firestore().collection("userData").doc(uid).collection("bodyTracking").orderBy("timeStamp" , "desc").limit(10).get().then((doc) => {
+                let time = []
+                doc.forEach((doc) => {
+                    let D = new Date(doc.data().timeStamp)
+                    const Months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+                    const datee = D.getDate()
+                    const month = Months[D.getMonth()]
+                    const year = D.getFullYear()
+
+                    let obj = {
+                        time : `${month} ${datee} ${year}`
+                    } 
+                    time.push(obj)
+                    console.log(obj)
+                    this.setState({time})
+                })
+            })
+        }
+
+      }
+    
 
     list = async() => {
 
@@ -40,7 +75,7 @@ export default class ProgressScreen extends Component{
                     weightEntry: doc.data().weightEntry  
                 }
                 weightEntry.push(obj)
-              console.log(obj)
+              //console.log(obj)
             
               this.setState({ weightEntry })
             })
@@ -62,33 +97,27 @@ export default class ProgressScreen extends Component{
          
     }
 
-  
-
-
-    progress = async() => {
-        let uid = await firebase.auth().currentUser.uid
+    // progress = async() => {
+    //     let uid = await firebase.auth().currentUser.uid
         
-         await firebase.firestore().collection("userData").doc(uid).collection("bodyTracking").orderBy("weightEntry", "desc").limit(17).get().then((querySnapshot) => {
+    //      await firebase.firestore().collection("userData").doc(uid).collection("bodyTracking").orderBy("weightEntry", "desc").limit(17).get().then((querySnapshot) => {
                     
                 
-                    let progress = []
+    //                 let progress = []
                   
-                    querySnapshot.forEach((doc) => {
-                        let Obj = {
-                            progress: (doc.data().startingWeight) - ((doc.data().weightEntry))
-                        }
-                        progress.push(Obj)
-                        console.log(Obj)
-                        this.setState({progress})
-                    })
+    //                 querySnapshot.forEach((doc) => {
+    //                     let Obj = {
+    //                         progress: (doc.data().startingWeight) - ((doc.data().weightEntry))
+    //                     }
+    //                     progress.push(Obj)
+    //                    // console.log(Obj)
+    //                     this.setState({progress})
+    //                 })
 
-            })
+    //         })
             
-    }
+    // }
 
-   
-
-   
     time = async() => {
 
         let uid = await firebase.auth().currentUser.uid
@@ -98,8 +127,6 @@ export default class ProgressScreen extends Component{
            
         let timeStamp = []
        
-
-            
             querySnapshot.forEach((doc) => {
 
                 let d = new Date(doc.data().timeStamp)
@@ -112,20 +139,66 @@ export default class ProgressScreen extends Component{
                 }
                
             timeStamp.push(obj)
-            console.log(obj)
+            //console.log(obj)
             this.setState({timeStamp})
             })
         })
 
     }
+
+    renderIndex(){
+        
+        return(
+
+            <ScrollView>
+            <View style={styles.container}>
+{/* 
+                <SegmentedControlTab
+                values={["Weight", "Measurement"]}
+                selectedIndex={this.state.selectedIndex}
+                onTabPress={this._handleIndexChange}
+
+                allowFontScaling={false}
+                tabsContainerStyle={styles.tabsContainerStyleFood}
+                tabStyle={styles.tabStyleFood}
+                firstTabStyle={styles.firstTabStyleFood}
+                lastTabStyle={styles.lastTabStyleFood}
+                tabTextStyle={styles.tabTextStyleFood}
+                activeTabStyle={styles.activeTabStyleFood}
+                activeTabTextStyle={styles.activeTabTextStyleFood}
+              /> */}
+              {/* {this.handleIndexChange(this.state.time)} */}
+            
+            </View>
+            </ScrollView>
+        )
+    
+}
+
  
     render(){
+        
+        
         return(
            
             <View style={styles.container}  >
-           
-            <Text>Weight</Text>
-           
+
+                <SegmentedControlTab
+                values={["Weight", "Measurement"]}
+                selectedIndex={this.state.selectedIndex}
+                onTabPress={this.handleIndexChange}
+
+                allowFontScaling={false}
+                tabsContainerStyle={styles.tabsContainerStyleFood}
+                tabStyle={styles.tabStyleFood}
+                firstTabStyle={styles.firstTabStyleFood}
+                lastTabStyle={styles.lastTabStyleFood}
+                tabTextStyle={styles.tabTextStyleFood}
+                activeTabStyle={styles.activeTabStyleFood}
+                activeTabTextStyle={styles.activeTabTextStyleFood}
+              />
+
+                
             <FlatList
               
                 data = {this.state.weightEntry}
