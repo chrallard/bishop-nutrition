384e3749ef3e7b7ba67eb14d100dcbf94d7c4641
Jeff Lichty <lich0020@algonquinlive.com>
2020-03-23 11:56:54 -0400

    top margin adjusted

--- widgets/summaryWidgets/MoodWidget.js
+++ widgets/summaryWidgets/MoodWidget.js
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
         padding: 16,
         alignSelf: 'stretch',
         marginBottom: 8,
-        marginTop: 44
+        marginTop: 16
     },
     title:{
         color:'#FAFAFA',
