24d5481e6b868f7e589dc481e85fbade934c6036
Jeff Lichty <lich0020@algonquinlive.com>
2020-03-23 11:55:40 -0400

    top margin adjusted

--- widgets/summaryWidgets/ActivityWidget.js
+++ widgets/summaryWidgets/ActivityWidget.js
@@ -118,7 +118,7 @@ const styles = StyleSheet.create({
         padding: 16,
         alignSelf: 'stretch',
         marginBottom: 8,
-        marginTop: 44
+        marginTop: 16
     },
     infoContainer:{
         flexDirection: 'row',
