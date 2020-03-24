bc49b3701b33d0e524773f973f56a3681fa05aac
Durbano <durb0004@algonqiunlive.com>
2020-03-24 18:50:00 -0400

    fixed bug

--- screens/ProgressScreen.js
+++ screens/ProgressScreen.js
@@ -187,7 +187,7 @@ export default class ProgressScreen extends Component {
 
     }
     _renderWeightContent = () => {
-        this.state.weightEntry.reverse(function (a, b) { return a.timeStamp - b.timeStamp })
+        this.state.weightEntry.sort(function (a, b) { return a.timeStamp - b.timeStamp })
         return (
             this.state.weightEntry.map((item, key) => (
                 <View key={key} style={{ margin: 20 }}>
@@ -199,7 +199,7 @@ export default class ProgressScreen extends Component {
         )
     }
     _renderMeasuermentsContent = () => {
-        this.state.measurements.reverse(function (a, b) { return a.timeStamp - b.timeStamp })
+        this.state.measurements.sort(function (a, b) { return a.timeStamp - b.timeStamp })
         return (
             this.state.measurements.map((item, key) => (
                 <View key={key} style={{ margin: 20 }}>
@@ -262,6 +262,10 @@ export default class ProgressScreen extends Component {
                             activeTabTextStyle={segmented.activeTabTextStyle}
                         />
                         {this._renderMeasuermentsContent()}
+
+
+
+
                     </View>
                 </ScrollView>
 
