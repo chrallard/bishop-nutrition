9ef30bbbdedea68c93835ca674f4c368cc3645ef
Conor Scott <scot0359@algonquinlive.com>
2020-03-26 16:09:24 -0400

    update password now modal & styled

--- screens/UpdatePasswordScreen.js
+++ screens/UpdatePasswordScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Button, TextInput, TouchableOpacity, Text } from 'react-native';
 import * as firebase from 'firebase/app'
 
 export default class UpdatePasswordScreen extends Component {
@@ -29,25 +29,30 @@ export default class UpdatePasswordScreen extends Component {
     render(){
         return(
             <View style={styles.container} >
-                <TextInput
-                style={styles.input}
-                placeholder="Current password"
-                onChangeText={(text) => this.setState({currentPasswordInput: text})}
-                value={this.state.currentPasswordInput}
-                secureTextEntry={true}
-                />
-                <TextInput
-                style={styles.input}
-                placeholder="New password"
-                onChangeText={(text) => this.setState({newPasswordInput: text})}
-                value={this.state.newPasswordInput}
-                secureTextEntry={true}
-                />
-                <View style={styles.btn}>
-                    <Button 
-                    title="Submit" 
-                    onPress={this.updatePassword}
+                <View style={styles.input}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="Current password"
+                    placeholderTextColor="#DDDEDE"
+                    onChangeText={(text) => this.setState({currentPasswordInput: text})}
+                    value={this.state.currentPasswordInput}
+                    secureTextEntry={true}
                     />
+                </View>
+                <View style={styles.input}>
+                    <TextInput
+                    style={styles.textInput}
+                    placeholder="New password"
+                    placeholderTextColor="#DDDEDE"
+                    onChangeText={(text) => this.setState({newPasswordInput: text})}
+                    value={this.state.newPasswordInput}
+                    secureTextEntry={true}
+                    />
+                </View>
+                <View style={styles.submitBtn}>
+                    <TouchableOpacity title="Submit" onPress={this.updatePassword} style={styles.submitTouchable}>
+                    <Text style={styles.submitBtnText}>Submit</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         )
@@ -57,20 +62,49 @@ export default class UpdatePasswordScreen extends Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#000000',
       alignItems: 'center',
       justifyContent: 'center',
       paddingBottom: 150
     },
     input: {
-      height: 40,
-      width: 200,
-      paddingLeft: 5,
-      borderBottomColor: "#505050",
-      borderBottomWidth: 1,
-      margin: 5
+        height: 55,
+        width: '75%',
+        paddingLeft: 5,
+        borderRadius: 5,
+        margin: 5,
+        backgroundColor: '#1C1C1E',
+        marginBottom: 16,
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+    textInput: {
+        flex: 1,
+        color: '#DDDEDE',
+        fontSize: 15
     },
-    btn: {
-        paddingTop: 50
-    }
+    submitBtn: {
+        marginTop: 45,
+        borderRadius: 28,
+        backgroundColor: '#347EFB',
+        height: 55,
+        width: '75%',
+        justifyContent: "center",
+        alignContent: 'center',
+        marginBottom: 20,
+        shadowColor: '#347EFB',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.6,
+        shadowRadius: 15,
+      },
+      submitBtnText: {
+        color: '#FAFAFA'
+      },
+      submitTouchable: {
+        height: '100%', 
+        width: '100%', 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center'
+      },
 })
