05b92d8fc91fd6a5cc855f1797f20cf38b0e567d
alla0243 <43067479+ChristianAllardAC@users.noreply.github.com>
2020-03-27 15:16:56 -0400

    removed average start and end sleep times

--- widgets/summaryWidgets/SleepWidget.js
+++ widgets/summaryWidgets/SleepWidget.js
@@ -6,9 +6,7 @@ export default class SleepWidget extends Component {
     constructor(props){
         super(props)
         this.state = {
-            avgSleep: 0,
-            avgStart: 0,
-            avgEnd: 0
+            avgSleep: 0
         }
     }
 
@@ -28,15 +26,11 @@ export default class SleepWidget extends Component {
 
         this.setState({
             avgSleep: this.props.sleepEntry.duration,
-            avgStart: this.msToTime(start + 1000 * 60 * 60 * 4),
-            avgEnd: this.msToTime(end + 1000 * 60 * 60 * 4)
         })
     }
 
     buildUpdatedSleep = () => {
         let newAvgSleep
-        let newAvgStart
-        let newAvgEnd
         let totalSleepDuration = 0
         let totalStartTime = 0
         let nullEntries = 0
@@ -54,9 +48,7 @@ export default class SleepWidget extends Component {
         newAvgSleep = totalSleepDuration / ((numberOfSleepEntries) == 0 ? 1 : numberOfSleepEntries) //if the user hasn't entered sleep yet, numberOfSleepEntries will be 0. you can't divide by 0, so it returns 1 instead
 
         this.setState({ 
-            avgSleep: this.msToDuration(newAvgSleep),
-            avgStart: 0,
-            avgEnd: 0
+            avgSleep: this.msToDuration(newAvgSleep)
         })
     }
 
@@ -87,19 +79,6 @@ export default class SleepWidget extends Component {
 
                 <Text style={styles.amountText}>{this.state.avgSleep}</Text>
                 <Text style={styles.titleText}>Average time asleep</Text>
-
-                <View style={styles.averageTextLayout}>
-
-                    <View style={{flexDirection: 'row'}}>
-                        <Text style={styles.averageTitleText}>Average start: </Text>
-                        <Text style={styles.averageAmountText}>{this.state.avgStart}</Text>
-                    </View>
-
-                    <View style={{flexDirection: 'row'}}>
-                        <Text style={styles.averageTitleText}>Average end: </Text>
-                        <Text style={styles.averageAmountText}>{this.state.avgEnd}</Text>
-                    </View>
-                </View>
             </View>
         )
     }
