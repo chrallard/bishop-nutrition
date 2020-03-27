31356bc2ec6f250a03c57e8ae3d8d3c9551bec0c
alla0243 <43067479+ChristianAllardAC@users.noreply.github.com>
2020-03-27 18:28:59 -0400

    line graph is displayed

--- screens/ProgressScreen.js
+++ screens/ProgressScreen.js
@@ -50,10 +50,6 @@ export default class ProgressScreen extends Component {
 
     }
 
-
-    
-   
-
     addModal = () => {
         this.refs.addModal.showModal();
     }
@@ -235,10 +231,8 @@ export default class ProgressScreen extends Component {
         )
 
     }
+
     render() {
-        const axesSvg = { fontSize: 10, fill: 'grey' };
-        const verticalContentInset = { top: 10, bottom: 10 }
-        const xAxisHeight = 30
         let time = []
         let weightEntry = []
         
@@ -247,38 +241,54 @@ export default class ProgressScreen extends Component {
            
         if (this.state.selectedIndex == 0) {
             
+            /////////////FROM DOCS
+            const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+            const axesSvg = { fontSize: 10, fill: '#F3F3F3' };
+            const verticalContentInset = { top: 10, bottom: 10 }
+            const xAxisHeight = 30
+            /////////////
+
             return (
                 //this is where you build the weight screen
-                
                 
                 <ScrollView>
                     <View style={styles.container}  >
 
-                    <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
-                <YAxis
-                    data={weightEntry}
-                    style={{ marginBottom: xAxisHeight }}
-                    contentInset={verticalContentInset}
-                    svg={axesSvg}
-                />
-                <View style={{ flex: 1, marginLeft: 10 }}>
-                    <LineChart
-                        style={{ flex: 1 }}
-                        data={weightEntry}
-                        contentInset={verticalContentInset}
-                        svg={{ stroke: 'rgb(134, 65, 244)' }}
-                    >
-                        <Grid/>
-                    </LineChart>
-                    <XAxis
-                        style={{ marginHorizontal: -10, height: xAxisHeight }}
-                        data={time}
-                        formatLabel={(value, index) => index}
-                        contentInset={{ left: 10, right: 10 }}
-                        svg={axesSvg}
-                    />
-                </View>
-            </View>
+                        <View style={{ height: 200, padding: 10, flexDirection: 'row', backgroundColor: '#347EFB' }}>
+
+                            <YAxis
+                                data={data}
+                                style={{ marginBottom: xAxisHeight, }}
+                                contentInset={verticalContentInset}
+                                svg={axesSvg}
+                            />
+
+                            <View style={{ flex: 1, marginLeft: 10, }}>
+                                <LineChart
+                                    style={{ flex: 1 }}
+                                    data={data}
+                                    contentInset={verticalContentInset}
+                                    svg={{ 
+                                        stroke: '#F3F3F3',
+                                        strokeWidth: 3
+                                    }}
+                                >
+
+                                    <Grid style={{ color: 'white', borderBottomWidth: 50, borderColor: 'red' }} />
+
+                                </LineChart>
+
+                                <XAxis
+                                    style={{ marginHorizontal: -10, height: xAxisHeight }}
+                                    data={data}
+                                    formatLabel={(value, index) => index}
+                                    contentInset={{ left: 10, right: 10 }}
+                                    svg={axesSvg}
+                                />
+                            </View>
+
+                        </View>
 
                             <SegmentedControlTab
                             values={["Weight", "Measurement"]}
